f3eee07c8ee695efdaf752d0f7419c9022c6ecd3
mgrouch <mgrushinskiy@gmail.com>
2021-08-31 00:35:26 -0400

    Revert "Update profiles.h"
    This reverts commit 59335a73

--- profiles.h
+++ profiles.h
@@ -248,12 +248,5 @@ Profile defaultProfile = {	.windowClass = NULL,
 				.tapAction = { ACTIONTYPE_BUTTONPRESS, 3, 0	}
 			  };
 
-Profile noopProfile = {	        .windowClass = NULL,
-				.scrollInherit = 1,
-				.zoomInherit = 1,
-				.rotateInherit = 1,
-				.tapInherit = 1
-			  };
-
 
 #endif /* PROFILES_H_ */
