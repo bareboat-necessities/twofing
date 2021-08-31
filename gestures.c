d0e1b2388e1b73aa6537144d0e63fba301708c05
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2021-08-30 22:00:57 -0400

    Update gestures.c

--- gestures.c
+++ gestures.c
@@ -557,7 +557,7 @@ Profile* getWindowProfile(Window w) {
 			if(inDebugMode()) {
 				printf("Current window: '%s'\n", class);
 			}
-			if (!strncmp(class, "opencpn", 30)) {
+			if (strncmp(class, "opencpn", 30) == 0) {
 				free(class);
 				return &defaultProfile;
 			}
